061764ec25e5879acb0d307ad77173aba7729696
peaceshi <peaceshi@outlook.com>
2021-10-17 19:45:04 +0800

    feat(pages): :sparkles: add client side only functions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,27 @@
-import React, { ReactElement } from "react";
 import "@styles/index.sass";
-import type { AppProps } from "next/app";
+import { checkWebpFeature, pwaHelper } from "@utils/utils";
 import { appWithTranslation } from "next-i18next";
+import type { AppProps } from "next/app";
 import Head from "next/head";
+import React, { ReactElement } from "react";
 
+const MyAppHead = (): ReactElement => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Web Map</title>
+  </Head>
+);
+const clientSideFunction = () => {
+  // Because of the way Next.js handles SSR,
+  // make sure your function is being called in client side only.
+  void checkWebpFeature();
+  pwaHelper();
+};
 const MyApp = ({ Component, pageProps }: AppProps): ReactElement => {
+  clientSideFunction();
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Web Map</title>
-      </Head>
+      <MyAppHead />
       <Component {...pageProps} />
     </>
   );
