59e71a5a064475957cc7d0746f692994b9cfdd27
peaceshi <peaceshi@outlook.com>
2021-10-17 19:43:03 +0800

    refactor(lib): :sparkles: map tile version control and fetch options

--- lib/fetchData.ts
+++ lib/fetchData.ts
@@ -2,6 +2,7 @@ import { load } from "@loaders.gl/core";
 import { ImageLoader } from "@loaders.gl/images";
 import { JSONLoader } from "@loaders.gl/json";
 import { FeatureCollection } from "@nebula.gl/edit-modes";
+import { checkGameMap } from "@utils/utils";
 import { Tile } from "./Interface";
 
 export const fetchDziMeta = async (dziSource: string): Promise<{ height: number; width: number; tileSize: number }> => {
@@ -17,21 +18,31 @@ export const fetchDziMeta = async (dziSource: string): Promise<{ height: number;
   return { height, width, tileSize };
 };
 
+const version = `?version=${checkGameMap()}`;
+const fetchOptions = { fetch: { headers: { Accept: "image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8" } } };
+const baseUrl = `https://assets.yuanshen.site`;
+
 export const fetchTileData = async ({ x, y, z }: Pick<Tile, "x" | "y" | "z">, tileUrlIndex: number): Promise<[]> => {
   switch (tileUrlIndex) {
     case 1:
-      return (await load(`https://assets.yuanshen.site/tiles_qd/${z + 14}/${x}_${y}.jpg`, ImageLoader)) as Promise<[]>;
+      return (await load(
+        `${baseUrl}/tiles_qd1/${z + 14}/${x}_${y}.jpg${version}?region=qd`,
+        ImageLoader,
+        fetchOptions
+      )) as Promise<[]>;
     case 2:
-      return (await load(`https://assets.yuanshen.site/tiles_qd1/${z + 14}/${x}_${y}.jpg`, ImageLoader)) as Promise<[]>;
+      return (await load(
+        `${baseUrl}/tiles_qd1/${z + 14}/${x}_${y}.jpg${version}?region=qd1`,
+        ImageLoader,
+        fetchOptions
+      )) as Promise<[]>;
     default:
-      // return (await load(`https://assets.yuanshen.site/tiles/${z + 14}/${x}_${y}.jpg`, ImageLoader)) as Promise<[]>;
-      return (await load(`https://assets.yuanshen.site/tiles_dq2/${z + 13}/${x}_${y}.jpg`, ImageLoader)) as Promise<[]>;
+      return (await load(
+        `${baseUrl}/tiles_dq3/${z + 13}/${x}_${y}.jpg${version}?region=Teyvat`,
+        ImageLoader,
+        fetchOptions
+      )) as Promise<[]>;
   }
-  // return (await load(`https://assets.yuanshen.site/tiles/${z + 14}/${x}_${y}.jpg`, ImageLoader)) as Promise<[]>;
-  // return (await load(
-  //   `http://ysmap.projectxero.top/mapdata/yuanshen.site/tiles/${z + 14}/${x}_${y}.jpg`,
-  //   ImageLoader
-  // )) as Promise<[]>;
 };
 export const fetchGeoJson = async (url: string): Promise<FeatureCollection> => {
   return (await load(url, JSONLoader)) as Promise<FeatureCollection>;
