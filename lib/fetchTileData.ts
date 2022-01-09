bdbb1b3bda02ee2d76afa0ab532fe2171db04c2d
peaceshi <peaceshi@outlook.com>
2022-01-09 23:19:07 +0800

    refactor(lib): :recycle: update tile url

--- lib/fetchTileData.ts
+++ lib/fetchTileData.ts
@@ -1,12 +1,9 @@
 import type { Tile } from "@lib/Interface";
 import { load } from "@loaders.gl/core";
 import { ImageLoader } from "@loaders.gl/images";
-import { checkGameMap } from "@utils";
-
 type TileXYZ = Pick<Tile, "x" | "y" | "z">;
 
 type TileMeta = TileJSON;
-const version = `${checkGameMap()}`;
 
 const fetchOptions = {
   fetch: {
@@ -14,9 +11,9 @@ const fetchOptions = {
   }
 };
 export const fetchTileData = async ({ x, y, z }: TileXYZ, Meta: TileMeta): Promise<[]> => {
-  const url = `${Meta.baseUrl}/${Meta.tileName}/${z + Meta.tileLevel}/${x}_${y}.jpg?version=${version}&region=${
-    Meta.tileRegion
-  }`;
+  const url = `${Meta.baseUrl}/${Meta.tileName}/${z + Meta.tileLevel}/${x}_${y}.${Meta.extName}?version=${
+    Meta.version
+  }&region=${Meta.tileRegion}`;
   return localStorage.getItem("webp") === "true"
     ? ((await load(url, ImageLoader, fetchOptions)) as Promise<[]>)
     : ((await load(url, ImageLoader)) as Promise<[]>);
