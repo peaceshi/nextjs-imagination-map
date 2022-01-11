4bd1ab91d5baacc4bee17aba53ddc37e651e79cb
peaceshi <peaceshi@outlook.com>
2022-01-11 22:40:20 +0800

    refactor(pages): change type name

--- pages/api/data/tiles/[[...meta]].ts
+++ pages/api/data/tiles/[[...meta]].ts
@@ -2,21 +2,21 @@ import { fetchJSON } from "@lib/fetchData";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const url = "https://assets.yuanshen.site/data/json/tileMeta.json";
-type Response = NextApiResponse<TileJSON | TileMetaJSONCollection | { error: string }>;
+type Response = NextApiResponse<TileJSON | TileJSONCollection | { error: string }>;
 type QueryRequest = NextApiRequest["query"];
-const querySelector = (data: TileMetaJSONCollection, query: QueryRequest, response: Response) => {
+const querySelector = (data: TileJSONCollection, query: QueryRequest, response: Response) => {
   if (query.meta == undefined) {
     return response.status(200).json(data);
   } else if (query.meta[0]) {
     switch (query.meta[0].toString()) {
       case "qd":
-        return response.status(200).json(data.qd as TileJSON);
+        return response.status(200).json(data.qd);
       case "qd1":
-        return response.status(200).json(data.qd1 as TileJSON);
+        return response.status(200).json(data.qd1);
       case "dq3":
       case "editor":
       default:
-        return response.status(200).json(data.dq3 as TileJSON);
+        return response.status(200).json(data.dq3);
     }
   } else {
     return response.status(400).json({ error: "Invalid meta" });
@@ -24,9 +24,9 @@ const querySelector = (data: TileMetaJSONCollection, query: QueryRequest, respon
 };
 const API = async (
   request: NextApiRequest,
-  response: NextApiResponse<TileJSON | TileMetaJSONCollection | { error: string }>
+  response: NextApiResponse<TileJSON | TileJSONCollection | { error: string }>
 ) => {
-  const data = await fetchJSON<TileMetaJSONCollection>(url);
+  const data = await fetchJSON<TileJSONCollection>(url);
   const { query } = request;
   querySelector(data, query, response);
 };
