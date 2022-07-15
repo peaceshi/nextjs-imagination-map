02a5e83cd55a549ecd5c909718344e94364ea234
peaceshi <peaceshi@outlook.com>
2022-07-15 23:44:09 +0800

    fix(pages): :bug: add referrer header

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,7 @@ export default class AppDocument extends Document {
         <Head>
           <meta name="application-name" content={APP_NAME} />
           <meta name="description" content={APP_DESCRIPTION} />
-
+          <meta name="referrer" content="never" />
           <meta name="apple-mobile-web-app-title" content={APP_NAME} />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
